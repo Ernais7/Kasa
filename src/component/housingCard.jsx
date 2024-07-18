625fb147515c3dcf1b2850725b6a430d645e7191
Ernais <tonybno@yahoo.fr>
2024-07-18 20:51:36 +0200

    feat: adding home page housing

---
+++ src/component/housingCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../style/sass/component/housing.scss";
+
+const HousingCard = ({ housing }) => {
+  const { title, cover, id } = housing;
+
+  if (!title || !cover || !id) {
+    return null;
+  }
+
+  return (
+    <Link to={`/Housing/${id}`} className="housing__card--link">
+      <img src={cover} alt={title} className='housing__card--image' />
+      <div className="housing__card--title-content">
+      <h3 className="housing__card--title">{title}</h3>
+      </div>
+    </Link>
+  );
+};
+
+export default HousingCard;
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 import Header from "../../component/Header/header.jsx";
 import SceneHome from "../../assets/paysageHome.png";
-import '../../style/sass/pages/index.scss';
+import "../../style/sass/pages/index.scss";
+import HousingData from "../../data/housingData.json";
+import HousingCard from "../../component/housingCard.jsx"
 
 function Home() {
   return (
     <div>
       <Header />
       <div className="poster">
-        <img src= {SceneHome} alt="Paysage de présentation" className="poster__image" />
+        <img src={SceneHome} alt="Paysage de présentation" className="poster__image" />
         <div className="poster__text">Chez vous, partout et ailleurs</div>
-        </div>
-      {}
+      </div>
+      <div className="housing">
+        {HousingData.map(housing => (
+          <div className="housing__card" key={housing.id}>
+            <HousingCard housing={housing} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default Home;
+
+
+
+
+
