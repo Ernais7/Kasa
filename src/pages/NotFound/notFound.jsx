b80e283c0425fcb72394994cf39925c4e1740f2a
Ernais <tonybno@yahoo.fr>
2024-07-25 16:46:16 +0200

    feat: adding not found page

--- src/pages/NotFound/notFound.jsx
+++ src/pages/NotFound/notFound.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import Header from "../../component/Header/header.jsx";
-import "../../style/sass/pages/index.scss";
+import "../../style/sass/pages/notFound.scss";
 import Footer from "../../component/Footer/footer.jsx";
 
 function NotFound() {
   return (
     <div>
       <Header />
-      <div>"Error"</div>
+      <div className='error__container'>
+            <h1 className='error__container--title'>404</h1>
+            <p className='error__container--text'>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/" className='error__container--link'>Retourner sur la page d'accueil</Link>
+        </div>
       <Footer />
     </div>
   );
